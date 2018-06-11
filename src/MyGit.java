b7b3627ba5769adfc4ac15a39bfbac371d27ecfb
Pgoud <praveen.goud@hotmail.com>
2018-06-11 12:41:26 +0530

    First commit.

---
+++ src/MyGit.java
@@ -0,0 +1,12 @@
+
+
+public class MyGit {
+
+   public final static void main(String[] args) {
+	
+	System.out.println("My Git ! Praveen -- Standalone");
+System.out.println("My Git ! Modified Praveen -- Standalone");
+
+   }
+
+}
