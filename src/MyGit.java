66b16b5038f9bec2d31a630f5f58ad85c68f0c57
Pgoud <praveen.goud@hotmail.com>
2018-06-11 13:15:48 +0530

    Second commit.

--- src/MyGit.java
+++ src/MyGit.java
@@ -5,7 +5,7 @@ public class MyGit {
    public final static void main(String[] args) {
 	
 	System.out.println("My Git ! Praveen -- Standalone");
-System.out.println("My Git ! Modified Praveen -- Standalone");
+System.out.println("My Git ! Modified Praveen -- Standalone -- Local WT");
 
    }
 
